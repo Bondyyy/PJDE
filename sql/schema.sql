89f7e6e2a9040554ab58b02ab9c8502d1afe6081
Bondyyy <safaridung987@gmail.com>
2026-08-13 07:44:38 +0700

    chỉnh schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,13 +1,15 @@
 CREATE TABLE IF NOT EXISTS users (
-    user_id BIGINT,
+    user_id BIGINT NOT NULL,
     login VARCHAR(255) NOT NULL,
     gravatar_id VARCHAR(255),
     url VARCHAR(255),
-    avatar_url VARCHAR(255)
+    avatar_url VARCHAR(255),
+    PRIMARY KEY (user_id)
 );
 
 CREATE TABLE IF NOT EXISTS repositories (
-    repo_id BIGINT,
+    repo_id BIGINT NOT NULL,
     name VARCHAR(255) NOT NULL,
-    url VARCHAR(255)
+    url VARCHAR(255),
+    PRIMARY KEY (repo_id)
 );
